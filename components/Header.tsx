3045495277dcc8a0fc6414a1eee112eccdc9bece
Adekoya <oladayo@spotter.ai>
2025-10-30 17:31:34 +0100

    implement header

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "./NavItems";
+import UserDropdown from "./UserDropdown";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            height={32}
+            width={140}
+            alt="daystock logo"
+            className="h-8 cursor-pointer w-auto"
+          />
+        </Link>
+        <nav className="hidden sm:block">
+          <NavItems />
+        </nav>
+        <UserDropdown />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
